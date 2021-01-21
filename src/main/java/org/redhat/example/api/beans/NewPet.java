8a11b0a4759c7af93347eba3df1f89f817ff0bca
cmhocm <41146912+cmhocm@users.noreply.github.com>
2021-01-21 16:41:18 +0800

    Add pet owner (java)

--- src/main/java/org/redhat/example/api/beans/NewPet.java
+++ src/main/java/org/redhat/example/api/beans/NewPet.java
@@ -3,12 +3,14 @@ package org.redhat.example.api.beans;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
     "name",
-    "tag"
+    "tag",
+    "owner"
 })
 public class NewPet {
 
@@ -21,6 +23,13 @@ public class NewPet {
     private String name;
     @JsonProperty("tag")
     private String tag;
+    /**
+     * Owner of this pet
+     * 
+     */
+    @JsonProperty("owner")
+    @JsonPropertyDescription("Owner of this pet")
+    private String owner;
 
     /**
      * 
@@ -50,6 +59,24 @@ public class NewPet {
     @JsonProperty("tag")
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    /**
+     * Owner of this pet
+     * 
+     */
+    @JsonProperty("owner")
+    public String getOwner() {
+        return owner;
+    }
+
+    /**
+     * Owner of this pet
+     * 
+     */
+    @JsonProperty("owner")
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 
 }
